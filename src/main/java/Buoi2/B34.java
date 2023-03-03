caa3244e8c1188d0216cf8720c216e10ba745892
nguyenmanhtu231199 <nguyenmanhtu231199@gmail.com>
2023-03-03 14:01:25 +0700

    update

---
+++ src/main/java/Buoi2/B34.java
@@ -0,0 +1,21 @@
+package Buoi2;
+
+import java.util.Scanner;
+
+public class B34 {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.print("Nhap n = ");
+        int n= in.nextInt();
+        in.close();
+        if (n<1) {
+            System.out.print("ERROR");
+            System.exit(0);
+        }
+        double t=0;
+        for (int i= 1; i<=n ; i++) {
+            t= Math.sqrt(i + t);
+        }
+        System.out.println("S(n) = " + t);
+    }
+}
